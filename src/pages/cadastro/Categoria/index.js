315a5de07efe143dceb4fdd2caf28ef30b13a1df
eneas-hsn <eneashsn@gmail.com>
2020-07-29 13:53:58 -0300

    Carrousel e formulário

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -6,12 +6,27 @@ import { Link } from 'react-router-dom';
 function CadastroCategoria() {
     return (
         <PageDefault>
-            <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria</h1>
 
-            <Link to="/">
-                Home
-            </Link>
-        </PageDefault>
+      <form>
+
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
     );
 };
 
